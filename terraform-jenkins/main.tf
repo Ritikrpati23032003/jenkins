da484f7d79f9bda81a43266ff49408dbd02e1b8d
Ritik Ranja Pati <ritikranjanpati@gmail.com>
2025-11-20 18:30:39 +0530

    day-01

---
+++ terraform-jenkins/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-07860a2d7eb515d9a"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "test-jenkins"
+    }
+}
