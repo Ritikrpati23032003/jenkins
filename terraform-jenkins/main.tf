df54ce6aaf80c713f569d1c79584bf02f6b6ad60
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2025-12-18 19:22:57 +0530

    webhook

--- terraform-jenkins/main.tf
+++ terraform-jenkins/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "name" {
     ami = "ami-07860a2d7eb515d9a"
-    instance_type = "t3.micro"
+    instance_type = "t3.medium"
     tags = {
         Name = "test-jenkins"
     }
