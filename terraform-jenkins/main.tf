8ed5a04cf220aed1dd55901d520a8b1dcb62b582
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2025-12-21 15:11:01 +0530

    Update main.tf

--- terraform-jenkins/main.tf
+++ terraform-jenkins/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "name" {
     ami = "ami-07860a2d7eb515d9a"
     instance_type = "t3.medium"
     tags = {
-        Name = "test-jenkins"
+        Name = "terraform-jenkins"
     }
 }
